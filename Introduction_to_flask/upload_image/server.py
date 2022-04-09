391df447f998d9f833915181368ab8bc19ba6811
Elmir <94553249+RussianPlay@users.noreply.github.com>
2022-04-09 03:34:42 +0300

    feature 1.1

--- Introduction_to_flask/upload_image/server.py
+++ Introduction_to_flask/upload_image/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 
 
 app = Flask(__name__)
@@ -15,34 +15,7 @@ def form_sample():
 
 
 def get_form_design():
-    return f'''<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                        <link rel="stylesheet"
-                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                        crossorigin="anonymous">
-                        <link rel='stylesheet' type='text/css' href='{url_for('static', filename='css/style.css')}' />
-                        <title>Отбор астронавтов</title>
-                      </head>
-                      <body>
-                        <h1>Загрузка фотографии</h1>
-                        <h2>для участия в миссии</h1>
-                        <div>
-                          <form method="post" enctype="multipart/form-data">
-                            <div class="form-group">
-                              <label for="photo">Выберите файл</label>
-                              <image src={url_for('static', filename="img/buffer_img.jpg")}>
-                              <input type="file" class="form-control-file" id="photo" name="file" method='post' 
-                              accept='image/*'>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Отправить</button>
-                          </form>
-                        </div>
-                      </body>
-                    </html>'''
+    return render_template("base.html")
 
 
 if __name__ == "__main__":
